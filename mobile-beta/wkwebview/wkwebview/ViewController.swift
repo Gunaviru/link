b277527fa3088b46256ae347d8f6d7bae2244a06
Paolo Bernasconi <paolo@plaid.com>
2016-10-31 16:16:38 -0700

    Initial example for beta/wkwebview

---
+++ mobile-beta/wkwebview/wkwebview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wkwebview
+//
+//  Created by Paolo Bernasconi on 10/27/16.
+//  Copyright © 2016 Plaid LLC. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
